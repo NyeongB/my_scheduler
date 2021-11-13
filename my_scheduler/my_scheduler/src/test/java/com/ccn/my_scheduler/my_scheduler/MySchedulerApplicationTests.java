25a8b8da25be5588729b577e0eb633de34c24aa1
CheolNyeong <cjfsud23@gmail.com>
2021-11-13 21:06:38 +0900

    프로젝트 세팅

---
+++ my_scheduler/my_scheduler/src/test/java/com/ccn/my_scheduler/my_scheduler/MySchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccn.my_scheduler.my_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
